df4a9f5490db6d8a6b2e6ab5df2f57b34eebc291
Rajput <nitin98811234@gmail.com>
2024-02-16 19:04:32 +0530

    Getter and Setter

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -38,8 +38,63 @@ Person.prototype.species = 'Homo Sepiens';
 // console.log(this);
 
 // Prototype Chain and Inheritance
-console.dir(Person);
+// console.dir(Person);
 
+// ES6 Classes
+//Class Expression
+// const PersonCl = class {
+
+// }
+//Class Declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  //Methods will be added to the prototype property of PersonCl class
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hello ${this.firstName}`);
+  }
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not valid`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1991);
+console.log(jessica.fullName);
+const nitin = new PersonCl('Nitin Rajput', 1998);
+console.log(nitin.fullName);
+// jessica.fullName('Nitin Rajput');
+// jessica.calcAge();
+// jessica.greet();
+// console.log(PersonCl.prototype === jessica.__proto__);
+
+//Getters and Setters
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 100, 300, 400],
+  get latest() {
+    return this.movements[this.movements.length - 1];
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+account.latest = 0;
+console.log(account.latest);
+
+// #####################################################
 // Coding Challenge #1
 // const Car = function (make, speed) {
 //   this.make = make;
