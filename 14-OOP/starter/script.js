44abf6e9a2f0f4416c87b43f107f82f3e92dd780
Rajput <nitin98811234@gmail.com>
2024-02-18 18:27:11 +0530

    Coding challenge #3

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -127,28 +127,28 @@
 // mamta.init('Mamta Rajput', 1960);
 // mamta.calcAge();
 // Inheritance between classes: constructor functions
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
-};
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
 
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
 
-const Student = function (firstName, birthYear, course) {
-  Person.call(this, firstName, birthYear);
-  this.course = course;
-};
-Student.prototype = Object.create(Person.prototype);
-Student.prototype.constructor = Student;
-Student.prototype.introduce = function () {
-  console.log(`My name is ${this.firstName} and I study ${this.course}`);
-};
+// const Student = function (firstName, birthYear, course) {
+//   Person.call(this, firstName, birthYear);
+//   this.course = course;
+// };
+// Student.prototype = Object.create(Person.prototype);
+// Student.prototype.constructor = Student;
+// Student.prototype.introduce = function () {
+//   console.log(`My name is ${this.firstName} and I study ${this.course}`);
+// };
 
-const mike = new Student('Mike', 2020, 'Computer Science');
-mike.introduce();
-mike.calcAge();
+// const mike = new Student('Mike', 2020, 'Computer Science');
+// mike.introduce();
+// mike.calcAge();
 
 // #####################################################
 // Coding Challenge #1
@@ -175,33 +175,71 @@ mike.calcAge();
 // mercedes.brake();
 
 // Coding Challenge #2
-class CarCl {
-  constructor(make, speed) {
-    this.make = make;
-    this.speed = speed;
-  }
+// class CarCl {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
 
-  accelerate() {
-    this.speed += 10;
-    console.log(`The new speed of ${this.make} is ${this.speed} Km/h`);
-  }
-  brake() {
-    this.speed -= 5;
-    console.log(`New speed of the car after brake : ${this.speed} Km/h`);
-  }
+//   accelerate() {
+//     this.speed += 10;
+//     console.log(`The new speed of ${this.make} is ${this.speed} Km/h`);
+//   }
+//   brake() {
+//     this.speed -= 5;
+//     console.log(`New speed of the car after brake : ${this.speed} Km/h`);
+//   }
 
-  get speedUS() {
-    return this.speed / 1.6;
-  }
+//   get speedUS() {
+//     return this.speed / 1.6;
+//   }
 
-  set speedUS(speed) {
-    this.speed = speed * 1.6;
-  }
-}
+//   set speedUS(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
 
-const ford = new CarCl('Ford', 120);
-ford.accelerate();
-ford.brake();
-console.log(ford.speedUS);
-ford.speedUS = 100;
-console.log(ford.speed);
+// const ford = new CarCl('Ford', 120);
+// ford.accelerate();
+// ford.brake();
+// console.log(ford.speedUS);
+// ford.speedUS = 100;
+// console.log(ford.speed);
+
+// Coding Challenge #3
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`New speed of the car after accelerate : ${this.speed} Km/h`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`New speed of the car after brake : ${this.speed} Km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+// Linking Prototypes
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.constructor = EV;
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= 1;
+  console.log(
+    `${this.make} going at ${this.speed}Km/h,with a charge of ${this.charge}%`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.accelerate();
+tesla.brake();
+tesla.chargeBattery(90);
