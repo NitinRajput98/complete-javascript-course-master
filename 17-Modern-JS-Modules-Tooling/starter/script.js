7c2cdae23f87424df6208c2f928502d681ddd61a
Rajput <nitin98811234@gmail.com>
2024-03-22 22:15:33 +0530

    writing clean code

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,9 +1,28 @@
-// import cart_One from './shopingCart.js';
-// import { addCart as cartAdd } from './shopingCart.js';
-// cartAdd('Apple');
-// cartAdd('Grape');
-// // console.log(my);
-// console.log(cart_One);
+import cart_One from './shopingCart.js';
+import { addCart as cartAdd } from './shopingCart.js';
+cartAdd('Apple');
+cartAdd('Grape');
+console.log(cart_One);
+//Hot module replacement
+if (module.hot) {
+  module.hot.accept();
+}
 
-import { addCart, cart } from './shopingCart.js';
-console.log(addCart);
+// import { addCart, cart } from './shopingCart.js';
+// console.log(addCart);
+
+// const result = (function () {
+//   const cart = [];
+//   const customVariable = 'Hello';
+
+//   const addCart = function (product) {
+//     cart.push(product);
+//   };
+
+//   return {
+//     cart,
+//     addCart,
+//   };
+// })();
+
+// console.log(result);
