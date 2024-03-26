66d72d0baba282cd4758ee02e4fccf6f2c7744ec
Rajput <nitin98811234@gmail.com>
2024-03-26 12:32:47 +0530

    exercise 9

--- 09-Data-Structures-Operators/starter/assignment.js
+++ 09-Data-Structures-Operators/starter/assignment.js
@@ -369,3 +369,30 @@ for (let book of books) {
 //   }
 // }
 console.log(allAuthors);
+//8.3
+for (const [index, author] of allAuthors.entries()) {
+  console.log(`${index + 1}. ${author}`);
+}
+
+//Enhanced Object Literals
+//9.1
+const bookData = [
+  ['title', 'Computer Networking: A Top-Down Approach'],
+  ['author', ['James F. Kurose', 'Keith W. Ross']],
+  ['publisher', 'Addison Wesley'],
+];
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: bookData[1][1],
+  [bookData[2][0]]: bookData[2][1],
+};
+console.log(newBook);
+//9.2
+const pages = 880;
+
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+  pages,
+};
+console.log(newBook2);
