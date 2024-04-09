521cc10f9348c980e22111d81c77ebe60696ed0b
Rajput <nitin98811234@gmail.com>
2024-04-09 22:54:50 +0530

    Forkify application completed

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,42 +1,55 @@
-import * as model from './model';
-import recipeView from './views/recipeView';
-import searchView from './views/searchView';
-import resultsView from './views/resultsView';
-import paginationView from './views/paginationView';
-import bookmarksView from './views/bookmarksView';
-import addRecipeView from './views/addRecipeView';
-import 'core-js/stable'; // Polyfilling everything else
-import 'regenerator-runtime/runtime'; // Polyfilling async/await/
-import { MODAL_CLOSE_SEC } from './config';
+import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
-const controlRecipes = async () => {
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
+
+const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
+
     if (!id) return;
     recipeView.renderSpinner();
+
+    // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+
+    // 1) Updating bookmarks view
     bookmarksView.update(model.state.bookmarks);
-    // 1) Loading Recipe
+
+    // 2) Loading recipe
     await model.loadRecipe(id);
 
-    // 2) Displaying Recipe
+    // 3) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
+    console.error(err);
   }
 };
 
-const controlSearchResults = async () => {
+const controlSearchResults = async function () {
   try {
     resultsView.renderSpinner();
-    // Get search query
+
+    // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
-    // Load search results
+
+    // 2) Load search results
     await model.loadSearchResults(query);
-    // Render Results
+
+    // 3) Render results
     resultsView.render(model.getSearchResultsPage());
-    // Render initital pagination buttons
+
+    // 4) Render initial pagination buttons
     paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
@@ -44,39 +57,68 @@ const controlSearchResults = async () => {
 };
 
 const controlPagination = function (goToPage) {
-  //1) Render Results
+  // 1) Render NEW results
   resultsView.render(model.getSearchResultsPage(goToPage));
-  //2) Render initital pagination buttons
+
+  // 2) Render NEW pagination buttons
   paginationView.render(model.state.search);
 };
 
 const controlServings = function (newServings) {
+  // Update the recipe servings (in state)
   model.updateServings(newServings);
-  // recipeView.render(model.state.recipe);
+
+  // Update the recipe view
   recipeView.update(model.state.recipe);
 };
+
 const controlAddBookmark = function () {
+  // 1) Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
+
+  // 2) Update recipe view
   recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
   bookmarksView.render(model.state.bookmarks);
 };
+
 const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
+
 const controlAddRecipe = async function (newRecipe) {
   try {
+    // Show loading spinner
     addRecipeView.renderSpinner();
+
+    // Upload the new recipe data
     await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    // Render recipe
     recipeView.render(model.state.recipe);
+
+    // Success message
     addRecipeView.renderMessage();
+
+    // Render bookmark view
+    bookmarksView.render(model.state.bookmarks);
+
+    // Change ID in URL
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+    // Close form window
     setTimeout(function () {
       addRecipeView.toggleWindow();
     }, MODAL_CLOSE_SEC * 1000);
   } catch (err) {
+    console.error('💥', err);
     addRecipeView.renderError(err.message);
   }
 };
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -86,5 +128,4 @@ const init = function () {
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
 };
-
 init();
--- 18-forkify/starter/src/js/views/View.js
+++ 18-forkify/starter/src/js/views/View.js
@@ -1,16 +1,30 @@
-import icons from 'url:../../img/icons.svg';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 export default class View {
   _data;
+
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author Jonas Schmedtmann
+   * @todo Finish implementation
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
+
     this._data = data;
     const markup = this._generateMarkup();
+
     if (!render) return markup;
+
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
+
   update(data) {
     this._data = data;
     const newMarkup = this._generateMarkup();
@@ -21,6 +35,7 @@ export default class View {
 
     newElements.forEach((newEl, i) => {
       const curEl = curElements[i];
+      // console.log(curEl, newEl.isEqualNode(curEl));
 
       // Updates changed TEXT
       if (
@@ -38,46 +53,49 @@ export default class View {
         );
     });
   }
+
   _clear() {
     this._parentElement.innerHTML = '';
   }
+
   renderSpinner() {
     const markup = `
-              <div class="spinner">
-                <svg>
-                  <use href="${icons}#icon-loader"></use>
-                </svg>
-              </div>
-        `;
-    this._parentElement.innerHTML = '';
+      <div class="spinner">
+        <svg>
+          <use href="${icons}#icon-loader"></use>
+        </svg>
+      </div>
+    `;
+    this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
+
   renderError(message = this._errorMessage) {
     const markup = `
-        <div class="error">
-                <div>
-                  <svg>
-                    <use href="${icons}#icon-alert-triangle"></use>
-                  </svg>
-                </div>
-                <p>${message}</p>
-              </div>
-              `;
+      <div class="error">
+        <div>
+          <svg>
+            <use href="${icons}#icon-alert-triangle"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
   renderMessage(message = this._message) {
     const markup = `
-        <div class="message">
-                <div>
-                  <svg>
-                    <use href="${icons}#icon-alert-triangle"></use>
-                  </svg>
-                </div>
-                <p>${message}</p>
-              </div>
-              `;
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
